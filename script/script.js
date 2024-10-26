9283151a2bd1e7b5651ace8db6920a9bc0d97a78
Juan Guerra <juan.l.guerrar@gmail.com>
2024-10-26 17:19:20 -0500

    Version final todo ok

---
+++ script/script.js
@@ -0,0 +1,27 @@
+let btnRepositorioEncrip = document.getElementById("btn_repositorio_encrip").onclick = function(){
+    window.open("https://github.com/JuanGuerra22/ChallengeEncriptador", "_blank");
+}
+
+let btnDemoEncrip = document.getElementById("btn_demo_encrip").onclick = function(){
+    window.open("https://juanguerra22.github.io/ChallengeEncriptador/", "_blank");
+}
+
+let insta = document.getElementById("instagram").onclick = function(){
+    window.open("https://www.instagram.com/juan_l_guerra/profilecard/?igsh=MXUzc296Z3lkNXJrbg==", "_blank");
+}
+
+let github = document.getElementById("github").onclick = function(){
+    window.open("https://github.com/JuanGuerra22", "_blank");
+}
+
+let linkedin = document.getElementById("linkedin").onclick = function(){
+    window.open("https://www.linkedin.com/in/juan-luis-guerra-75b0a02ba/", "_blank");
+}
+
+let btnRepositorioJuego = document.getElementById("btn_repositorio_juego").onclick = function(){
+    window.open("https://github.com/JuanGuerra22/JuegoNumSecreto", "_blank");
+}
+        
+let btnDemoJuego = document.getElementById("btn_demo_juego").onclick = function(){
+    window.open("https://juanguerra22.github.io/JuegoNumSecreto/", "_blank");
+}
